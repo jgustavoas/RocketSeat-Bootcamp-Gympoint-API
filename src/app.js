0b4be10b8f47304b5db51a19589875c7d5892d71
Gustavo Alexandrino <jgustavoas@gmail.com>
2019-10-24 21:34:48 -0300

    Usando Sentry e Youch

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express, { json } from 'express';
-// import './database';
-
+import Youch from 'youch'; // Pretty error reporting for Node.js
+import * as Sentry from '@sentry/node';
+import 'express-async-errors'; // Necessário que seja importado antes de "routes"
 import routes from './routes';
+
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -9,17 +12,33 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(json());
   }
 
   // rotas também funcionam como middlewares e por isso podem ser chamadas dentro de "this.server.use"
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    // Esté é um middleware assíncrono para tratamento de exceção
+    // Observe quatro parâmetros ao invés de três
+    // O primeiro parâmetro é o erro e este vem primeiro para que seja capturado
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
